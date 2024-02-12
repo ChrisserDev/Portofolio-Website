6ff700fc02f9e8bcd8db46a7704c75ed80a11e43
ChrisserDev <126911205+ChrisserDev@users.noreply.github.com>
2024-02-12 19:39:16 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is the start</h1>
+    </>
+  )
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+  </>,
+)
+
